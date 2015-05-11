c930ca35df6810b3f9c8f6a9331f70bee0536032
Sam Cranford <sam@sam-zen.home>
2015-05-11 19:50:42 +0200

    have valid hands

--- poker/poker/Poker.cs
+++ poker/poker/Poker.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 
 namespace poker
 {
+
 	public class Poker
 	{
+		private List<char> suits = new List<char>(){'H', 'C', 'D','S'};
+		private List<char> values = new List<char>(){'A', '2', '3','4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
 		public Poker ()
 		{
+		}
+
+		bool isValidCard (string card)
+		{
+			if (card.Length != 2) {
+				return false;
+			}
+			return values.Contains (card [0]) && suits.Contains (card [1]);
 		}
 
 		public bool isValidHand(List<string> hand)
@@ -16,7 +27,8 @@ namespace poker
 			}
 
 			foreach(var card in hand){
-				if (card.Length != 2) {
+
+				if (!isValidCard (card)) {
 					return false;
 				}
 			}
